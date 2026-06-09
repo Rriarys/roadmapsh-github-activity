8e90f83532bde0550453a28e51fee25b9584fa38
rriarys <rriarys@gmail.com>
2026-06-09 12:46:53 +0300

    feat(github): add GitHubActivityClient and fetch user events closes #2
    Introduce GitHubActivityClient to retrieve user activity from the GitHub API asynchronously with error handling. Update Program.cs to display fetched activity after username input.

---
+++ GitHubActivityCli.App/GitHub/GitHubActivityClient.cs
@@ -0,0 +1,39 @@
+﻿namespace GitHubActivityCli.App.GitHub;
+
+internal class GitHubActivityClient
+{
+    // GET https://api.github.com/users/{username}/events
+
+    public async Task<string> GetUserActivityAsync(string username)
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("GitHubActivityCliApp");
+
+        var url = $"https://api.github.com/users/{username}/events";
+
+        try
+        {
+            var response = await httpClient.GetAsync(url);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("User not found");
+                return string.Empty;
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error fetching user activity: {ex.Message}");
+            return string.Empty;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected error: {ex.Message}");
+            return string.Empty;
+        }
+    }
+}
--- GitHubActivityCli.App/Program.cs
+++ GitHubActivityCli.App/Program.cs
@@ -1,5 +1,11 @@
 ﻿using GitHubActivityCli.App.Cli;
+using GitHubActivityCli.App.GitHub;
 
 string username = UserInput.GetGitHubUsername();
-
 Console.WriteLine(username);
+
+Console.WriteLine("\n-----------------------------------\n");
+
+var client = new GitHubActivityClient();
+var activity = await client.GetUserActivityAsync(username);
+Console.WriteLine(activity);
